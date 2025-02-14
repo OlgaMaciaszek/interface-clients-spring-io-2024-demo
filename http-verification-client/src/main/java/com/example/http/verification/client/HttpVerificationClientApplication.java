0bdee1fc674c5cb5c78483870ca90f799efea9fc
Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>
2025-02-14 16:30:38 +0100

    Adjust to changes in FW.

--- http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
+++ http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
@@ -4,11 +4,11 @@ import com.example.http.verification.client.clients.VerificationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.service.config.EnableInterfaceClient;
+import org.springframework.web.service.config.InterfaceClient;
 
 @SpringBootApplication
-//@EnableInterfaceClient(value = "http://localhost:8081", basePackageClasses = PersonService.class)
-@EnableInterfaceClient(value = "http://localhost:8081", name = "verificationClient", httpServiceTypes = {VerificationService.class})
+//@InterfaceClient(value = "http://localhost:8081", basePackageClasses = PersonService.class)
+@InterfaceClient(value = "http://localhost:8081", name = "verificationClient", httpServiceTypes = {VerificationService.class})
 public class HttpVerificationClientApplication {
 
 	public static void main(String[] args) {
